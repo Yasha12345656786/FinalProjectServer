dc057fbe1f4c3af3a306d86438206b014362c436
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-08-20 14:26:14 +0300

    update 20/08

--- models/memoryGame.js
+++ models/memoryGame.js
@@ -1,13 +1,13 @@
 const { ObjectId } = require('mongodb');
 const DB = require('../utils/db');
 class MemoryGame {
-   level;
+   levels;
    cards;
    minMoves;
    points;
 
     constructor(level, cards, minMoves, points) {
-        this.level = level;
+        this.levels = levels;
         this.cards = cards;
         this.minMoves = minMoves;
         this.points = points;
@@ -16,25 +16,33 @@ class MemoryGame {
         let query = {_id:new ObjectId(gameID)}
         return await new DB().FindOne('MemoryGame',query);
     }
+    static async GetMemoryGameLevel(gameID,lvl){
+       let query = this.GetGame(gameID);
+       let options = {
+        Levels: 
+            {$elemMatch:{lvl:lvl,query}}
+       }
+        return await new DB().FindAll('MemoryGame',options);
+    }
 
-    static async GetAllCards(gameID,) {
-        let memoryGame = GetGame(gameID);
-        let query = 
-        let options = {}
-        return await new DB().FindOne('MemoryGame', query, options);
-    }
-    static async GetCard(gameID){
-        let game = this.GetGame()
+    // static async GetAllCards(gameID,) {
+    //     let memoryGame = GetGame(gameID);
+    //     let query = 
+    //     let options = {}
+    //     return await new DB().FindOne('MemoryGame', query, options);
+    // }
+    // static async GetCard(gameID){
+    //     let game = this.GetGame()
 
-    }
-    static async AddCard(gameID,id,img){
-        let query = {_id: new ObjectId(gameID)}
-        let doc = {
-            id: id,
-            img:img
-        }
-        return await new DB().UpadateById('MemoryGame',query,doc)
-    }
+    // }
+    // static async AddCard(gameID,id,img){
+    //     let query = {_id: new ObjectId(gameID)}
+    //     let doc = {
+    //         id: id,
+    //         img:img
+    //     }
+    //     return await new DB().UpadateById('MemoryGame',query,doc)
+    // }
     static async GetMovesPerLevel(gameID){
         let query = {_id:new ObjectId(gameID)}
         let options = {projection:{MinMoves:1,_id:0}}
--- routes/memoryGame.route.js
+++ routes/memoryGame.route.js
@@ -1,45 +1,66 @@
 const memoryGame = require('../models/memoryGame');
 const MemoryRoute =  require('express').Router();
 
-MemoryRoute.get('/:id/cards', async (req,res)=>{
+
+MemoryRoute.get('/:id/MemoryGame', async(req,res)=>{
     try {
         let {id} = req.params;
-        let data = await memoryGame.GetAllCards(id);
+        let data = await memoryGame.GetGame(id);
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json({error});
     }
-
 });
-
-MemoryRoute.get('/:id/MinMoves',async (req,res)=>{
+MemoryRoute.get('/:id/:lvl/memoryGameLevel',async(req,res)=>{
     try {
         let {id} = req.params;
-        let data = await memoryGame.GetMovesPerLevel(id);
+        let {lvl} = req.params;
+        let data = await memoryGame.GetMemoryGameLevel(id,lvl);
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json({error});
-    }
-})
-
-MemoryRoute.put('/:id/AddCard',async(req,res)=>{
-    try {
-        let {_id} = req.params;
-        let {id} = req.body;
-        let {img} = req.body;
-        let data = await memoryGame.AddCard(_id,id,img)
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({error});
-        
     }
 });
-MemoryRoute.get('/:id/:Cardid/findCardByID', async(req,res)=>{
-    try {
+
+// MemoryRoute.get('/:id/cards', async (req,res)=>{
+//     try {
+//         let {id} = req.params;
+//         let data = await memoryGame.GetAllCards(id);
+//         res.status(200).json(data);
+//     } catch (error) {
+//         res.status(500).json({error});
+//     }
+
+// });
+
+// MemoryRoute.get('/:id/MinMoves',async (req,res)=>{
+//     try {
+//         let {id} = req.params;
+//         let data = await memoryGame.GetMovesPerLevel(id);
+//         res.status(200).json(data);
+//     } catch (error) {
+//         res.status(500).json({error});
+//     }
+// })
+
+// MemoryRoute.put('/:id/AddCard',async(req,res)=>{
+//     try {
+//         let {_id} = req.params;
+//         let {id} = req.body;
+//         let {img} = req.body;
+//         let data = await memoryGame.AddCard(_id,id,img)
+//         res.status(200).json(data);
+//     } catch (error) {
+//         res.status(500).json({error});
         
-    } catch (error) {
+//     }
+// });
+// MemoryRoute.get('/:id/:Cardid/findCardByID', async(req,res)=>{
+//     try {
         
-    }
-})
+//     } catch (error) {
+        
+//     }
+// })
 
 module.exports = MemoryRoute;
