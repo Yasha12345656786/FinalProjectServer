bca57e0fde6c91f1f5f571dd2eb72a4774cb307e
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-08-28 14:36:32 +0300

    update 28/08

--- Client/src/Context/AdminContext.jsx
+++ Client/src/Context/AdminContext.jsx
@@ -1,89 +1,80 @@
 import { createContext, useState, useEffect } from "react";
 
 export const AdminContext = createContext();
-export default  function AdminContextProvider({children}){
-
-    const [admin, setAdmin] = useState({});
-const Login = async (username,password)=>{
+export default function AdminContextProvider({ children }) {
+  const [admin, setAdmin] = useState({});
+  const Login = async (username, password) => {
     try {
-        let response = await fetch('/api/admin/login', {
-            method:'POST',
-            headers:{
-                Accept:"application/json",
-                "Content-Type":"application/json",
-            },
-            body: JSON.stringify({username,password})
-        });
-        if (response.ok) {
-            let data = await response.json();
-            setAdmin(data);
+      let response = await fetch(
+        `${import.meta.env.VITE_SERVER_PATH}/api/admin/login`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
         }
-         
+      );
+      if (response.ok) {
+        let data = await response.json();
+        setAdmin(data);
+        return true;
+      }
     } catch (error) {
-        
+      //setNotifcation("error has occured");
+      //logService.send(error)
+      console.error(error);
     }
-}
-const UpdateUsername = async (username)=>{
+  };
+  const UpdateUsername = async (username) => {
     try {
-        let response = await fetch('/api/admin/updateUsername',{
-            method:'PUT',
-            headers:{'Content-Type':'application/json'},
-            body:JSON.stringify({username})
-        });
-        if (response.ok) {
-            let data = await response.json();
-            setAdmin(data);
-            
-        }
-    } catch (error) {
-        
-    }
-}
-const UpdatePassword = async (password)=>{
+      let response = await fetch("/api/admin/updateUsername", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username }),
+      });
+      if (response.ok) {
+        let data = await response.json();
+        setAdmin(data);
+      }
+    } catch (error) {}
+  };
+  const UpdatePassword = async (password) => {
     try {
-        let response = await fetch('/api/admin/updatePassword',{
-            method:'PUT',
-            headers:{'Content-Type':'application/json'},
-            body:JSON.stringify({password})
-        });
-        if (response.ok) {
-            let data = await response.json();
-            setAdmin(data);
-            
-        }
-    } catch (error) {
-        
-    }
-}
-const GetAdminById = async (id) =>{
+      let response = await fetch("/api/admin/updatePassword", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ password }),
+      });
+      if (response.ok) {
+        let data = await response.json();
+        setAdmin(data);
+      }
+    } catch (error) {}
+  };
+  const GetAdminById = async (id) => {
     try {
-        let response = await fetch('/api/admin/getAdminById',{
-            method:'GET',
-            body:JSON.stringfy({admin})
-        });
-        if(response.ok){
-            let data = await response.json();
-            setAdmin(data);
-        }
-    } catch (error) {
-        
-    }
+      let response = await fetch("/api/admin/getAdminById", {
+        method: "GET",
+        body: JSON.stringfy({ admin }),
+      });
+      if (response.ok) {
+        let data = await response.json();
+        setAdmin(data);
+      }
+    } catch (error) {}
+  };
+
+  const value = {
+    admin,
+    Login,
+    UpdateUsername,
+    UpdatePassword,
+    GetAdminById,
+  };
+
+  return (
+    <AdminContext.Provider value={value}>{children}</AdminContext.Provider>
+  );
 }
-
-
-    const value = {
-        admin,
-        Login,
-        UpdateUsername,
-        UpdatePassword,
-        GetAdminById
-    }
-
-    return(
-        <AdminContext.Provider value = {value}>
-            {children}
-        </AdminContext.Provider>
-    )
-
-}
-
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,55 +1,46 @@
-import React, {useState} from 'react'
+import React, { useContext, useState } from "react";
+import { AdminContext } from "../Context/AdminContext";
+import { redirect } from "react-router-dom";
 
 export default function Login() {
-    const [username, SetUsername] = useState('');
-    const [password, SetPassword] = useState('');
-
-  async function Submit(event){
-         event.preventDefault();
-         CheckUserName();
-         CheckPassWord();
-         let admin = {username, password};
-        let data = await res.json();
-        console.log(data);
-        alert('welcome' + data.username);
-    }
-    const CheckUserName=()=>{
-        check = false;
-        if(username.length < 10 && username.length > 0){
-            check = true;
-        } 
-        for (let index = 0; index < username.length; index++) {
-          const element = username[index];
-          if (element >= 'A' || element <='Z' && element >= 'a' || element <='z' && element >= '0' || element <= '9' ) {
-            continue;
-          }
-          else{check = false
-             return check;}      
-        }
-
-    }
-    const CheckPassWord=()=>{
-        check = false;
-    if (password.length >= 7 || password.length <= 12) {
-        check = true;
-    }
-    count = 0;
-    for (let index = 0; index < password.length; index++) {
-       if (element >= 'A' && element <='Z' || element >= 'a' && element <='z' && element >= '0' || element <= '9' )  {
-         count++;
-         check = true;
-       }           
-    }
-    return check;
-    }
+  const [username, SetUsername] = useState('');
+  const [password, SetPassword] = useState('');
+  async function Submit(event) {
+    event.preventDefault();
+    let admin = { username, password };
+    let res = await fetch("http://5500/api/admin/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(admin),
+    });
+    let data = await res.json();
+    console.log(data);
+    // const isSuccesful = await Login(username, password);
+    // if (isSuccesful) {
+    //   return redirect("/TriviaGameMenu");
+    // }
+  }
   return (
     <div>
-        <h1>Login</h1>
-        <form onSubmit={(event)=>Submit(event)}>
-            <input type='text' placeholder='username' required onChange={(event)=>SetUsername(event.target.value)}/>
-            <input type='password' placeholder='password' required onChange={(event)=>SetPassword(event.target.value)}/>
-            <button type="submit">Login</button>
-        </form>
+      <h1>Login</h1>
+      <form onSubmit={Submit}>
+        <input
+          type="text"
+          placeholder="username"
+          required
+          onChange={(event) => SetUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          required
+          onChange={(event) => SetPassword(event.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
+  );
 }
