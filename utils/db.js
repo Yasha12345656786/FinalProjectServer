d2b59058c9a78516993a2be49f13c0c05c03b5b6
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-09-07 16:28:27 +0300

    up

--- utils/db.js
+++ utils/db.js
@@ -1,82 +1,81 @@
-const {MongoClient, ObjectId} = require('mongodb');
-class DB{
-    db_uri;
-    db_name;
-    client;
+const { MongoClient, ObjectId } = require("mongodb");
+class DB {
+  db_uri;
+  db_name;
+  client;
 
-    constructor(){
-        
-        this.db_uri = process.env.DB_URI;
-        this.db_name = process.env.DB_NAME;
-        this.client = new MongoClient(this.db_uri);
-    }
+  constructor() {
+    this.db_uri = process.env.DB_URI;
+    this.db_name = process.env.DB_NAME;
+    this.client = new MongoClient(this.db_uri);
+  }
 
-    async FindAll(collection, query = {}, options = {}){
-        try {
-             
-            await this.client.connect();
-            return await this.client.db(this.db_name).collection(collection).find(query, options).toArray();
-        } catch (error) {
-            throw error;
-        }
-        finally{
-            await this.client.close();
-        }
-        
+  async FindAll(collection, query = {}, options = {}) {
+    try {
+      await this.client.connect();
+      return await this.client
+        .db(this.db_name)
+        .collection(collection)
+        .find(query, options)
+        .toArray();
+    } catch (error) {
+      throw error;
+    } finally {
+      await this.client.close();
     }
-    async FindOne(collection,  query = {}, options = {}){
-        try {
-            await this.client.connect();
-            return await this.client.db(this.db_name).collection(collection).findOne(query, options);
-        } catch (error) {
-            throw error;
-        }
-        finally{
-            await this.client.close();
-        }
-        
+  }
+  async FindOne(collection, query = {}, options = {}) {
+    try {
+      await this.client.connect();
+      return await this.client
+        .db(this.db_name)
+        .collection(collection)
+        .findOne(query, options);
+    } catch (error) {
+      throw error;
+    } finally {
+      await this.client.close();
     }
-    async Insert(collection, doc){
-        try {
-            await this.client.connect();
-            return await this.client.db(this.db_name).collection(collection).insertOne(doc);
-        } catch (error) {
-            throw error;
-        }
-        finally{
-            await this.client.close();
-        }
+  }
+  async Insert(collection, doc) {
+    try {
+      await this.client.connect();
+      return await this.client
+        .db(this.db_name)
+        .collection(collection)
+        .insertOne(doc);
+    } catch (error) {
+      throw error;
+    } finally {
+      await this.client.close();
     }
-    async UpdateById(collection,id,doc){
-        try {
-            await this.client.connect();
-            console.log(id, doc);
-            return await this.client.db(this.db_name).collection(collection).updateOne(
-                {_id: new ObjectId(id)},
-                {$set:{...doc}});
-        } catch (error) {
-            throw error;
-        }
-        finally{
-            await this.client.close();
-
-        }
-
-
+  }
+  async UpdateById(collection, id, doc) {
+    try {
+      await this.client.connect();
+      console.log(id, doc);
+      return await this.client
+        .db(this.db_name)
+        .collection(collection)
+        .updateOne({ _id: new ObjectId(id) }, { $set: { ...doc } });
+    } catch (error) {
+      throw error;
+    } finally {
+      await this.client.close();
     }
-    async FindOneById(collection,  id){
-        try {
-            await this.client.connect();
-            return await this.client.db(this.db_name).collection(collection).findOne({_id:new ObjectId(id)});
-        } catch (error) {
-            throw error;
-        }
-        finally{
-            await this.client.close();
-        }
-        
+  }
+  async FindOneById(collection, id) {
+    try {
+      await this.client.connect();
+      return await this.client
+        .db(this.db_name)
+        .collection(collection)
+        .findOne({ _id: new ObjectId(id) });
+    } catch (error) {
+      throw error;
+    } finally {
+      await this.client.close();
     }
+  }
 }
 module.exports = DB;
-
-
