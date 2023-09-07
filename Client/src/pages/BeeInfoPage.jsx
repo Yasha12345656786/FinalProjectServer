acf41e98788a8d5b59690eda3f50131a992fb4e7
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-09-07 17:05:41 +0300

    Update BeeInfoPage.jsx

--- Client/src/pages/BeeInfoPage.jsx
+++ Client/src/pages/BeeInfoPage.jsx
@@ -1,28 +1,29 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
-import React from 'react'
+import React from "react";
 
 export default function BeeInfoPage() {
-    const [page,SetPage]=useState('');
+  const [page, SetPage] = useState("");
 
-    async function LoadData(){
-        try {
-            let res = await fetch('https://www.pestworldforkids.org/pest-guide/bees/');
-            let data = await res.json();
-            //SetPage(data);
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
+  async function LoadData() {
+    try {
+      let res = await fetch(
+        "https://en.wikipedia.org/wiki/Honey_bee#:~:text=While%20about%2020%2C000%20species%20of,)%3B%20Apis%20florea%20(the%20red"
+      );
+      let data = await res.json();
+      //SetPage(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
-    useEffect(()=>{
-      LoadData();
-    },[])
+  }
+  useEffect(() => {
+    LoadData();
+  }, []);
   return (
     <>
-    <h1>Bee Info Pages</h1>
-    <p>Here You Can Learn More About Bees...</p>
-
+      <h1>Bee Info Pages</h1>
+      <p>Here You Can Learn More About Bees...</p>
     </>
-  )
+  );
 }
