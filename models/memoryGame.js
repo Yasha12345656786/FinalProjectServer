7c0ebeaa33fdd8d6033afcdea10cb3fb1ab40c9f
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-09-10 18:08:22 +0300

    update 10/09

--- models/memoryGame.js
+++ models/memoryGame.js
@@ -12,43 +12,11 @@ class MemoryGame {
         this.MinMoves = MinMoves;
         this.points = points;
     }
-    static async GetGame(gameID){
-        let query = {_id:new ObjectId(gameID)}
-        return await new DB().FindOne('MemoryGame',query);
-    }
-    static async GetMemoryGameLevel(gameID,lvl){
-       //let query = this.GetGame(gameID);
-       let projection = {Levels:{$elemMatch:{lvl:2}}};
-       const queryAns = await new DB().FindOne('MemoryGame',projection,projection);
-        return queryAns;
-    }
-    // static async GetAllCards(gameID,) {
-    //     let memoryGame = GetGame(gameID);5
-    //     let query = 
-    //     let options = {}
-    //     return await new DB().FindOne('MemoryGame', query, options);
-    // }
-    // static async GetCard(gameID){
-    //     let game = this.GetGame()
-
-    // }
-    // static async AddCard(gameID,id,img){
-    //     let query = {_id: new ObjectId(gameID)}
-    //     let doc = {
-    //         id: id,
-    //         img:img
-    //     }
-    //     return await new DB().UpadateById('MemoryGame',query,doc)
-    // }
-    static async GetMovesPerLevel(gameID){
-        let query = {_id:new ObjectId(gameID)}
-        let options = {projection:{MinMoves:1,_id:0}}
+    
+    static async GetCards(lvl){
+        let query = {"lvl":Number(lvl)} 
+        let options  = {projection:{cards:1}}
         return await new DB().FindOne('MemoryGame',query,options);
-    }
-    static async GetCardById(gameID, cardID) {
-        let query = { "cardID": cardID }
-        return await new DB().FindOne('MemoryGame', query);
-
     }
     static async GetAllLevels(){
         return await new DB().FindAll('MemoryGame');
--- routes/memoryGame.route.js
+++ routes/memoryGame.route.js
@@ -1,74 +1,84 @@
-const memoryGame = require('../models/memoryGame');
-const MemoryRoute =  require('express').Router();
+const { default: MemoryGame } = require("../Client/src/pages/MemoryGame");
+const memoryGame = require("../models/memoryGame");
+const MemoryRoute = require("express").Router();
 
-MemoryRoute.get('/',async (req,res)=>{
-    try {
-        let data = await memoryGame.GetAllLevels();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({error});
-    }
+MemoryRoute.get("/", async (req, res) => {
+  try {
+    let data = await memoryGame.GetAllLevels();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
-MemoryRoute.get('/:lvl',async (req,res)=>{
-    try {
-        let {lvl} = req.params;
-        let data = await memoryGame.GetLevelBylvl(lvl);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({error});
-    }
+MemoryRoute.get("/:lvl", async (req, res) => {
+  try {
+    let { lvl } = req.params;
+    let data = await memoryGame.GetLevelBylvl(lvl);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
-MemoryRoute.get('/minmovesbyLvl/:lvl', async(req,res)=>{
-    try {
-        let {lvl} = req.params;
-        let data = await memoryGame.GetMinMovesBylvl(lvl);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({error});
-    }
+MemoryRoute.get("/minmovesbyLvl/:lvl", async (req, res) => {
+  try {
+    let { lvl } = req.params;
+    let data = await memoryGame.GetMinMovesBylvl(lvl);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
-MemoryRoute.get('/GetPointsByLvl/:lvl',async(req,res)=>{
-     try {
-        let {lvl} = req.params;
-        let data = await memoryGame.GetPointsByLvl(lvl);
-        res.status(200).json(data);
-     } catch (error) {
-        res.status(500).json({error});
-     }
+MemoryRoute.get("/GetPointsByLvl/:lvl", async (req, res) => {
+  try {
+    let { lvl } = req.params;
+    let data = await memoryGame.GetPointsByLvl(lvl);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
-MemoryRoute.post('/AddLevel',async(req,res)=>{
-    try {
-           let {lvl} = req.body;
-           let {Cards} = req.body;
-           let {MinMoves} = req.body;
-           let {points} = req.body;
-           let data = await memoryGame.AddLevel(lvl,Cards,MinMoves,points);
-           res.status(200).json(data);
-    } catch (error) {
-           res.status(500).json({error});
-    }
+MemoryRoute.post("/AddLevel", async (req, res) => {
+  try {
+    let { lvl } = req.body;
+    let { Cards } = req.body;
+    let { MinMoves } = req.body;
+    let { points } = req.body;
+    let data = await memoryGame.AddLevel(lvl, Cards, MinMoves, points);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 //tried it also with lvl
-MemoryRoute.put('/EditLevelById/:id',async(req,res)=>{
-    try {
-        let {id} = req.params;
-        let {lvl} = req.body;
-        let {Cards} = req.body;
-        let {MinMoves} = req.body;
-        let {points} = req.body;
-        let data = await memoryGame.EditLevelById(id, lvl, Cards, MinMoves, points);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({error});
-    }
+MemoryRoute.put("/EditLevelById/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    let { lvl } = req.body;
+    let { Cards } = req.body;
+    let { MinMoves } = req.body;
+    let { points } = req.body;
+    let data = await memoryGame.EditLevelById(id, lvl, Cards, MinMoves, points);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
-MemoryRoute.get('/GetNextGameByLvl/:lvl',async(req,res)=>{
-    try {
-        let{lvl}= req.params;
-        let game = await memoryGame.GetNextGameByLvl(Number(lvl));
-        res.status(200).json(game);
-    } catch (error) {
-        res.status(500).json({error});
-    }
-})
+MemoryRoute.get("/GetNextGameByLvl/:lvl", async (req, res) => {
+  try {
+    let { lvl } = req.params;
+    let game = await memoryGame.GetNextGameByLvl(Number(lvl));
+    res.status(200).json(game);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+MemoryGame.get("/GetCards/:lvl", async (req, res) => {
+  try {
+    let { lvl } = req.params;
+    let data = await memoryGame.GetCards(Number(lvl));
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 module.exports = MemoryRoute;
