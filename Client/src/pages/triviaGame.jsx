12c2559a74bb006143d94396635b100e64d6ebe9
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-09-24 18:39:07 +0300

    Update triviaGame.jsx

--- Client/src/pages/triviaGame.jsx
+++ Client/src/pages/triviaGame.jsx
@@ -3,47 +3,47 @@ import { TriviaContext } from "../Context/TriviaGameContext";
 import { AdminContext } from "../Context/AdminContext";
 
 export default function TriviaGame() {
-  const { currentQuestion, UpdateScore, question, GetQuestion } = useContext(TriviaContext);
-  const { admin } = useContext(AdminContext);
+//   const { currentQuestion, UpdateScore, question, GetQuestion } = useContext(TriviaContext);
+//   const { admin } = useContext(AdminContext);
   
   
-  const AnswerPressed = (answer) => {
-    if (!answer.correct) {
-      UpdateScore(admin._id, 0);
-    } else {
-      UpdateScore(admin._id, answer.points);
-    }
-  };
-  console.log(currentQuestion);
-  useEffect(() => {
-    GetQuestion();
-}, []);
-console.log("sadds",question);
-  function renderContent() {
-    return (
-      <>
-        {" "}
-        <h3>{currentQuestion?.lvl}</h3>
-        <h3>{admin.triviaScore}</h3>
-        <h1>{currentQuestion?.q}</h1>
-        <div>
-          {currentQuestion?.Answers?.map((answer, index) => {
-            console.log(answer);
-            return (
-              <button key={index} onProgress={() => AnswerPressed(answer)}>
-                <h5>{answer.value}</h5>
-              </button>
-            );
-          })}
-        </div>
-      </>
-    );
-  }
+//   const AnswerPressed = (answer) => {
+//     if (!answer.correct) {
+//       UpdateScore(admin._id, 0);
+//     } else {
+//       UpdateScore(admin._id, answer.points);
+//     }
+//   };
+//   console.log(currentQuestion);
+//   useEffect(() => {
+//     GetQuestion();
+// }, []);
+// console.log("sadds",question);
+  // function renderContent() {
+  //   return (
+  //     <>
+  //       {" "}
+  //       <h3>{currentQuestion?.lvl}</h3>
+  //       <h3>{admin.triviaScore}</h3>
+  //       <h1>{currentQuestion?.q}</h1>
+  //       <div>
+  //         {currentQuestion?.Answers?.map((answer, index) => {
+  //           console.log(answer);
+  //           return (
+  //             <button key={index} onProgress={() => AnswerPressed(answer)}>
+  //               <h5>{answer.value}</h5>
+  //             </button>
+  //           );
+  //         })}
+  //       </div>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
       <div>TriviaGame</div>
-      {!currentQuestion.lvl ? <div>loading</div> : renderContent()}
+      {/* {!currentQuestion.lvl ? <div>loading</div> : renderContent()} */}
     </>
   );
 }
