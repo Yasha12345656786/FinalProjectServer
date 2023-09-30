9bdd3557f0ff74bbf10efbd65ccc28a5c196f0e7
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-09-30 20:15:12 +0300

    Update player.js

--- models/player.js
+++ models/player.js
@@ -3,26 +3,26 @@ const bcrypt = require("bcrypt");
 class Player {
   first_name;
   last_name;
+  username;
   email;
   password;
-  username;
   triviaScore;
   memoryScore;
 
   constructor(
     first_name,
     last_name,
+    username,
     email,
     password,
-    username,
     triviaScore,
     memoryScore
   ) {
     this.first_name = first_name;
     this.last_name = last_name;
+    this.username = username;
     this.email = email;
     this.password = password;
-    this.username = username;
     this.triviaScore = triviaScore;
     this.memoryScore = memoryScore;
   }
